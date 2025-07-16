b90abe687e952ad7ca5a5539930a41621bea0c14
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2025-07-16 16:58:23 +0300

    добавил SecurityConfiguration

--- src/main/java/by/cargocontractservice/CargoContractServiceApplication.java
+++ src/main/java/by/cargocontractservice/CargoContractServiceApplication.java
@@ -1,7 +1,9 @@
 package by.cargocontractservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
---
+++ src/main/java/by/cargocontractservice/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package by.cargocontractservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers("/contracts/**").authenticated()
+                        .requestMatchers("/organizations/**").authenticated()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                );
+        return httpSecurity.build();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtAuthenticationConverter());
+        return converter;
+    }
+}
