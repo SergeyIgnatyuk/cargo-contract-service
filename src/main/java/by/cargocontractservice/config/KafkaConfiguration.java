7d5b03a79a63032c2f30510acfe5777c8e846a22
Sergey Ignatyuk <sergey.ignatyuk@jetbi.com>
2025-08-01 16:14:21 +0300

    добавил настройку ACKS_CONFIG для продюсера в кафка

--- src/main/java/by/cargocontractservice/config/KafkaConfiguration.java
+++ src/main/java/by/cargocontractservice/config/KafkaConfiguration.java
@@ -26,6 +26,7 @@ public class KafkaConfiguration {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
